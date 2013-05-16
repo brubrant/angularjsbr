00dcdaec18bb4e316959fee1940d3a361464384c
Bruno Brant <brubran@gmail.com>
2013-05-15 21:53:46 -0300

    Taxonomy Tipo transformada em Lang

--- chun-child/custom-post-types.php
+++ chun-child/custom-post-types.php
@@ -2,20 +2,20 @@
 
     
 	// now let's add custom tags (these act like categories)
-    register_taxonomy( 'tipo', 
+    register_taxonomy( 'idioma', 
     	array('post'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
-    	array('hierarchical' => false,    /* if this is false, it acts like tags */                
+    	array('hierarchical' => true,    /* if this is false, it acts like tags */                
     		'labels' => array(
-    			'name' => __( 'Tipos', 'bonestheme' ), /* name of the custom taxonomy */
-    			'singular_name' => __( 'Tipo', 'bonestheme' ), /* single taxonomy name */
-    			'search_items' =>  __( 'Buscar Tipos', 'bonestheme' ), /* search title for taxomony */
-    			'all_items' => __( 'Todos os Tipos', 'bonestheme' ), /* all title for taxonomies */
-    			'parent_item' => __( 'Tipo Pai', 'bonestheme' ), /* parent title for taxonomy */
-    			'parent_item_colon' => __( 'Tipo Pai:', 'bonestheme' ), /* parent taxonomy title */
-    			'edit_item' => __( 'Edit Tipo', 'bonestheme' ), /* edit custom taxonomy title */
-    			'update_item' => __( 'Update Tipo', 'bonestheme' ), /* update title for taxonomy */
-    			'add_new_item' => __( 'Adicionar novo Tipo', 'bonestheme' ), /* add new title for taxonomy */
-    			'new_item_name' => __( 'Novo nome de Tipo', 'bonestheme' ) /* name title for taxonomy */
+    			'name' => __( 'Idiomas', 'bonestheme' ), /* name of the custom taxonomy */
+    			'singular_name' => __( 'Idioma', 'bonestheme' ), /* single taxonomy name */
+    			'search_items' =>  __( 'Buscar Idiomas', 'bonestheme' ), /* search title for taxomony */
+    			'all_items' => __( 'Todos os Idiomas', 'bonestheme' ), /* all title for taxonomies */
+    			'parent_item' => __( 'Idioma Pai', 'bonestheme' ), /* parent title for taxonomy */
+    			'parent_item_colon' => __( 'Idioma Pai:', 'bonestheme' ), /* parent taxonomy title */
+    			'edit_item' => __( 'Edit Idioma', 'bonestheme' ), /* edit custom taxonomy title */
+    			'update_item' => __( 'Update Idioma', 'bonestheme' ), /* update title for taxonomy */
+    			'add_new_item' => __( 'Adicionar novo Idioma', 'bonestheme' ), /* add new title for taxonomy */
+    			'new_item_name' => __( 'Novo nome de Idioma', 'bonestheme' ) /* name title for taxonomy */
     		),
     		'show_admin_column' => true,
     		'show_ui' => true,
--- chun-child/functions.php
+++ chun-child/functions.php
@@ -19,6 +19,8 @@
 /* Add the child theme setup function to the 'after_setup_theme' hook. */
 add_action( 'after_setup_theme', 'chun_child_theme_setup', 11 );
 
+require_once('custom-post-types.php'); // Chama Snnipets de custom post types
+
 /**
  * Setup function.  All child themes should run their setup within this function.  The idea is to add/remove 
  * filters and actions after the parent theme has been set up.  This function provides you that opportunity.
@@ -37,5 +39,6 @@ function chun_child_theme_setup() {
 	// add_filter( "{$prefix}_site_title", 'chun_child_example_filter' );
 
 }
+
 
 ?>
